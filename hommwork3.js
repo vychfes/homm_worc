2e6494c4c972c1e529a5c252a63f5c2ee4e5270e
Vych Fes <94452999+vychfes@users.noreply.github.com>
2021-11-29 00:43:37 +0200

    Add files via upload

---
+++ hommwork3.js
@@ -0,0 +1,64 @@
+// function examinatioNum(num1) {
+//     if (isNaN(num1)) {
+//         return true
+//     }
+//     return false
+// }
+
+// function outputsMean(num1, num2, num3) {
+//     // num1 = +num1;
+//     // num2 = +num2;
+//     // num3 = +num3;
+//     if (num1 == "" || num2 == "" || num3 == "" || isNaN(+num1)|| isNaN(+num2) || isNaN(+num3) ) {
+//         return false
+//     }if (num3 > num2 && num2 > num1) {
+//         return num2
+//     } if (num1 > num2 && num2 > num3) {
+//         return num2
+//     } else if (num2 > num3 && num3 > num1) {
+//         return num3
+//     } else if (num1 > num3 && num3 > num2) {
+//         return num3
+//     } else if (num3 > num1 && num1 > num2) {
+//         return num1
+//     } else if (num2 > num1 && num1 > num3) {
+//         return num1
+//     }
+//
+// }
+
+
+// function examinationName() {
+//     let obj = {
+//         name: "john",
+//         age: 33
+//     }
+//     alert(name in obj)
+//
+// }
+//
+// console.log(examinationName())
+
+
+
+// function outputsUsername(){
+//     let obj = {
+//         name: 'Pieter',
+//         username: 'Snaps'
+//     }
+//         for(let key in obj)
+//         return
+//     {
+// alert(obj.name)
+//     }
+//
+//
+// }
+//
+// console.log(outputsUsername())
+function getSum(){
+    let sum = {name1: 200, name2: 500, name3: 400}
+}{
+    alert(`${getSum.name1} + ${getSum.name2}`)
+}
+console.log(getSum())
