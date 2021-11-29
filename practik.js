2789feddf5c743de3423dc61bfc65a2177a40421
Vych Fes <vychik1985@gmail.com>
2021-11-29 20:03:56 +0200

    practik work

---
+++ practik.js
@@ -0,0 +1,20 @@
+// function upFirst(name){
+//
+//     return (`${name[0].toUpperCase()}${name.slice(1).toLowerCase()}`)
+//
+// }
+//
+// console.log(upFirst('GTNHJD'))
+
+
+// function truncate(str, maxlength){
+//     if (str.length > maxlength){
+//         return (srt = str.slice(0, +maxlength + 1) + "...")
+//     }
+// }
+// console.log(truncate('gfhljkhlk', 5))
+
+
+function getShortName(fullName){
+      
+}
