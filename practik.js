0e46e88b9e342fd142eda193cc178157220a2786
Vych Fes <vychik1985@gmail.com>
2021-11-30 01:05:35 +0200

    practik modef

--- practik.js
+++ practik.js
@@ -15,6 +15,51 @@
 // console.log(truncate('gfhljkhlk', 5))
 
 
-function getShortName(fullName){
-      
+// function getShortName(fullName){
+//     let arr = ['иванов', 'сергей', 'петрович'];
+//     return (`${arr[0].slice(0,1).toUpperCase()}${arr[0].slice(1).toLowerCase()} ${arr[1].slice(0,1).toUpperCase()}.${arr[2].slice(0,1).toUpperCase()}.`)
+//
+// }
+//
+// console.log(getShortName())
+
+
+
+//     let arr = [
+//         {
+//             name: 'Pieter',
+//             age: 23,
+//             username: 'Pen'
+//
+//         },
+//         {
+//
+//             age: 28,
+//             username: 'John'
+//
+//         },
+//         {
+//             name: 'Mary',
+//             age: 18
+//
+//
+//         },
+//     ]
+// let getName= arr.map(function (item, index, array) {
+//     return item ? item.name: -1;
+//
+// });
+//
+// console.log(getName)
+
+
+
+// let arr0 = ['Pieter', 'john'];
+// let arr1 = ['Pen', 'Mary'];
+// const arr3 = [...arr0, ...arr1]
+// console.log(arr3)
+
+
+
+
 }
