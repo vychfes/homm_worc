2608e4dc854e36dcb0a8d0eb54951ca866acfd17
Vych Fes <vychik1985@gmail.com>
2021-12-10 20:21:16 +0200

    commit project Protection

--- projectsklad/projectsklad.js
+++ projectsklad/projectsklad.js
@@ -8,7 +8,7 @@ let unitUl = document.querySelector('.unit-product');
 if (getProducts) {
     for (let i = 0; i < getProducts.length; i++) {
         let listItem = document.createElement("li");
-        listItem.innerHTML = getProducts[i].name;
+        listItem.innerText= getProducts[i].name;
         productListUL.appendChild(listItem);
     }
     for (let i = 0; i < getProducts.length; i++) {
@@ -33,7 +33,7 @@ if (getProducts) {
     }
     for (let i = 0; i < getProducts.length; i++) {
         let listItem = document.createElement("li");
-        listItem.innerHTML = getProducts[i].select;
+        listItem.innerHTML = getProducts[i].unit;
         unitUl.appendChild(listItem);
     }
 }
@@ -127,7 +127,7 @@ button.onclick = function () {
     document.getElementById("nameProduct").value = '';
 }
 let buttonFilter = document.getElementById('buttfiltr');
-
+let result2 = [];
 buttonFilter.onclick = function () {
     let nameFilter = document.getElementById('name_filter').value;
     let numberFilter = document.getElementById('number_filter').value;
@@ -150,6 +150,7 @@ buttonFilter.onclick = function () {
             return el.unit === selectFilter;
         }
     })
+
     while (productListUL.firstChild) {
         productListUL.removeChild(productListUL.firstChild);
     }
@@ -199,9 +200,78 @@ buttonFilter.onclick = function () {
         }
         for (let i = 0; i < result.length; i++) {
             let listItem = document.createElement("li");
-            listItem.innerHTML = result[i].select;
+            listItem.innerHTML = result[i].unit;
             unitUl.appendChild(listItem);
         }
     }
 
+
+    while (productListUL.firstChild) {
+        productListUL.removeChild(productListUL.firstChild);
+    }
+    while (productNumUl.firstChild) {
+        productNumUl.removeChild(productNumUl.firstChild);
+    }
+    while (priceComingUl.firstChild) {
+        priceComingUl.removeChild(priceComingUl.firstChild);
+    }
+    while (priceRetailUl.firstChild) {
+        priceRetailUl.removeChild(priceRetailUl.firstChild);
+    }
+    while (priceWholesaleUl.firstChild) {
+        priceWholesaleUl.removeChild(priceWholesaleUl.firstChild);
+    }
+    while (unitUl.firstChild) {
+        unitUl.removeChild(unitUl.firstChild);
+    }
+    result2 = result.filter(function (el, index) {
+
+        if (numberFilter != '') {
+            return el.number === numberFilter;
+        } else if (comingFilter != '') {
+            return el.coming === comingFilter;
+        } else if (retailFilter != '') {
+            return el.retail === retailFilter;
+        } else if (saleFilter != '') {
+            return el.sale === saleFilter;
+        } else {
+            return el.unit === selectFilter;
+        }
+    })
+
+    if (result2) {
+        for (let i = 0; i < result2.length; i++) {
+            let listItem = document.createElement("li");
+            listItem.innerHTML = result2[i].name;
+            productListUL.appendChild(listItem);
+        }
+
+        for (let i = 0; i < result2.length; i++) {
+            let listItem = document.createElement("li");
+            listItem.innerHTML = result2[i].number;
+            productNumUl.appendChild(listItem);
+        }
+        for (let i = 0; i < result2.length; i++) {
+            let listItem = document.createElement("li");
+            listItem.innerHTML = result2[i].coming;
+            priceComingUl.appendChild(listItem);
+        }
+        for (let i = 0; i < result2.length; i++) {
+            let listItem = document.createElement("li");
+            listItem.innerHTML = result2[i].retail;
+            priceRetailUl.appendChild(listItem);
+        }
+        for (let i = 0; i < result2.length; i++) {
+            let listItem = document.createElement("li");
+            listItem.innerHTML = result2[i].sale;
+            priceWholesaleUl.appendChild(listItem);
+        }
+        for (let i = 0; i < result2.length; i++) {
+            let listItem = document.createElement("li");
+            listItem.innerHTML = result2[i].unit;
+            unitUl.appendChild(listItem);
+        }
+
+
+    }
 }
