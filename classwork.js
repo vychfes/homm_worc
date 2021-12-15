35f20de0ad18bc81f792a44178dd2216a2b4bd08
Vych Fes <vychik1985@gmail.com>
2021-12-16 00:43:20 +0200

    commit classWork end

--- classwork.js
+++ classwork.js
@@ -87,27 +87,100 @@
 // }
 
 
+let reMail = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/
+let reAge = /^(?:1(?:00?|\d)|[2-5]\d|[6-9]\d?)$/
+let rePass = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}/g
+let reLogin = /^[a-zA-z]{1}[a-zA-Z1-9]{5,20}$/
+let inputLogin = document.getElementById('login');
+let inputMail = document.getElementById('mail');
+let inputAge = document.getElementById('age');
+let inputPass = document.getElementById('pass');
+let inputPassConfirmation = document.getElementById('pass2');
+let btn = document.getElementById('form_butt');
+let formInputs = document.querySelectorAll('.js-input');
 
-
-let reMail = "/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/"
-let reAge = "/^\\+?[1-9][0-9]{0,1}$/"
-let rePass = '/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}/g'
-let reLogin = '/^[a-zA-z]{1}[a-zA-Z1-9]{3,20}$/'
-let btn = document.getElementById('butt');
-let inputLogin = document.getElementById('login').value;
-let inputMail = document.getElementById('mail').value;
-let inputAge = document.getElementById('age').value;
-let inputPass = document.getElementById('pass').value;
-let inputPassConfirmation = document.getElementById('pass2').value;
-
-document.getElementById("form_butt").onclick = function (){
-    // e.preventDefault();
-    if (reLogin.test(inputLogin)){
-        console.log('no')
-    }else{
-        console.log('yes')
-    }
+function validateEmail(email) {
+    return reMail.test(String(email).toLowerCase());
 }
+function validateLogin(login) {
+    return reLogin.test(String(login));
+}
+function validateAge(age) {
+    return reAge.test(String(age));
+}
+function validatePass(pass) {
+    return rePass.test(String(pass));
+}
+
+btn.onclick = function () {
+    let emailVal = inputMail.value,
+        loginVal = inputLogin.value,
+        ageVal = inputAge.value,
+        passVal = inputPass.value,
+        passCon = inputPassConfirmation.value,
+    spanText = document.getElementById('span')
+    emptyInputs = Array.from(formInputs).filter(input => input.value === '');
+    formInputs.forEach(function (input) {
+        if (input.value === '') {
+            input.classList.add('error')
+        } else {
+            input.classList.remove('error')
+        }
+    })
+    if (emptyInputs.length !== 0) {
+        console.log('input not error')
+        return false;
+    }
+    if (!validateEmail(emailVal)) {
+        console.log('email not valid');
+        inputMail.classList.add('error');
+        spanText.innerText = 'email в не верном формате';
+        return false;
+    } else {
+        inputMail.classList.remove('error')
+    }
+    if (!validateLogin(loginVal)) {
+        console.log('login not valid');
+        inputLogin.classList.add('error');
+        spanText.innerText = 'Не верно введен логин';
+        return false;
+    } else {
+        inputLogin.classList.remove('error');
+    }
+    if (!validateAge(ageVal)) {
+        console.log('age not valid');
+        inputAge.classList.add('error');
+        spanText.innerText = 'Укажите возраст 0-100';
+        return false;
+    } else {
+        inputAge.classList.remove('error');
+    }
+    if (!validatePass(passVal)) {
+        console.log('pass not valid');
+        inputPass.classList.add('error');
+        spanText.innerText = 'Пароль должен содержать цифры, буквы, заглавные буквы и спец.символ';
+        return false;
+    } else {
+        inputPass.classList.remove('error');
+    }
+    if (passCon != passVal)  {
+        console.log('pass not valid');
+        inputPassConfirmation.classList.add('error');
+        spanText.innerText = 'Пароль введен не верный';
+        return false;
+    } else {
+        inputPassConfirmation.classList.remove('error');
+        spanText.innerText = 'Форма отправлена успешно';
+
+    }
+    document.getElementById('login').value = '';
+    document.getElementById('age').value = '';
+    document.getElementById('mail').value = '';
+    document.getElementById('pass').value = '';
+    document.getElementById('pass2').value = '';
+
+}
+
 
 
 
